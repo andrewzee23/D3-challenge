bf89780b62fd12b18137ad1e2e9c37dbbb0eb761
andrewzee23 <andrewazamora@gmail.com>
2020-10-28 12:43:52 -0500

    setting chart width and height

---
+++ StarterCode/assets/js/app.js
@@ -0,0 +1,14 @@
+// Set height and width for svg
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+// Set chart width and height
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
