37a17b3d09cd90177fb1a41241ea644f5845775c
andrewzee23 <andrewazamora@gmail.com>
2020-10-28 12:52:22 -0500

    added function to change scale

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -24,3 +24,20 @@ var svg = d3
 var chartGroup = svg
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// axis params
+var chosenXAxis = "poverty";
+var chosenYAxis = "healthcare";
+
+// updating x/y scale when clicked
+function xScale(stateData, chosenXAxis) {
+  var xLinearScale = d3
+    .scaleLinear()
+    .domain([
+      d3.min(stateData, d => d[chosenXAxis]) * 0.8,
+      d3.max(stateData, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+}
